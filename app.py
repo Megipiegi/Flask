c51f3bbf62811989cde86adb95665ad9d166a783
Magda Pawłowska <magdalena.pawlowska5@gmail.com>
2021-07-21 12:09:45 +0200

    nowe repo

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template
+app = Flask (__name__)
+
+@app.route ('/mypage/me')
+def me():
+    return render_template ('me.html')
+@app.route('/mypage/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'GET':
+        print('We received GET')
+        return render_template('contact.html')
+    elif request.method == 'POST':
+        print('We received POST')
+        print(request.form)
+        return redirect("/")
